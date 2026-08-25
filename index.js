7cab521d3cd843844e10f464cf949fdfeefa1c15
mbiplob997-droid <mbiplob997@gmail.com>
2026-08-25 17:10:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.get('/proxy', async (req, res) => {
   const targetUrl = req.query.url;
   if (!targetUrl) return res.status(400).send('URL missing');
@@ -11,14 +19,18 @@ app.get('/proxy', async (req, res) => {
       method: 'get',
       url: targetUrl,
       headers: {
-        'User-Agent': 'okhttp/5.0.0-alpha.2'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Accept': '*/*'
       },
-      responseType: 'stream'
+      responseType: 'arraybuffer',
+      maxRedirects: 5,
+      timeout: 15000
     });
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', response.headers['content-type'] || 'application/x-mpegURL');
-    response.data.pipe(res);
+    const contentType = response.headers['content-type'];
+    if (contentType) res.setHeader('Content-Type', contentType);
+    
+    res.send(response.data);
   } catch (error) {
     res.status(500).send('Proxy error: ' + error.message);
   }
